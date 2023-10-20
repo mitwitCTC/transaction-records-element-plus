7b669875dc00794cbbb8f4e385e9a8d6bb5eaaa2
eileen-qian <yiqianc.14@gmail.com>
2023-10-20 10:17:04 +0800

    update: 儲值扣抵明細串接 API

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,185 +5,128 @@ import {
   ElButton,
   ElPagination,
 } from 'element-plus';
+
+const Api = 'https://5644-122-116-23-30.ngrok-free.app';
+
 export default {
   data() {
     return {
+      plate: '',
+      isLoading: true,
       transactions: [
         {
-          id: 1,
-          type: "折抵",
-          station: "西園1",
-          seller: "56632513",
-          plate: "XXX-111",
-          account: "ABC111",
-          balance: 450,
-          invoice: "QW123456789",
-          amount: 50,
-          randonCode: "111",
-          issueDate: "2023-10-01",
-          entryTime: "2023-10-01 11:11:11",
-          departTime: "2023-10-01 11:11:11",
-          buyer: "12345678"
+          "chargeType": "扣抵",
+          "name": "test",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 3235,
+          "invoice": "TE43427304",
+          "totalAmount": 40,
+          "randomCode": "7304",
+          "invoiceDate": "2023-10-11",
+          "arrivalTime": "2023-10-11 14:16:14",
+          "logTime": "2023-10-11 14:59:11",
+          "Bidentifier": null,
+          "BName": null
         },
         {
-          id: 2,
-          type: "儲值",
-          station: "西園2",
-          seller: "56632513",
-          plate: "XXX-222",
-          account: "ABC222",
-          balance: 1550,
-          invoice: "QW123456789",
-          amount: 1000,
-          randonCode: "222",
-          issueDate: "2023-10-01",
-          entryTime: "2023-10-01 12:11:11",
-          departTime: "2023-10-01 13:11:11",
-          buyer: "12345678"
+          "chargeType": "扣抵",
+          "name": "助安板橋殯儀館",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 3275,
+          "invoice": "TE43426936",
+          "totalAmount": 40,
+          "randomCode": "6936",
+          "invoiceDate": "2023-10-09",
+          "arrivalTime": "2023-10-09 07:23:18",
+          "logTime": "2023-10-09 08:15:45",
+          "Bidentifier": null,
+          "BName": null
         },
         {
-          id: 3,
-          type: "折抵",
-          station: "西園3",
-          seller: "56632513",
-          plate: "XXX-333",
-          account: "ABC333",
-          balance: 450,
-          invoice: "QW123456789",
-          amount: 50,
-          randonCode: "333",
-          issueDate: "2023-10-02",
-          entryTime: "2023-10-02 12:11:11",
-          departTime: "2023-10-02 14:11:11",
-          buyer: "12345678"
+          "chargeType": "儲值",
+          "name": "助安板橋殯儀館",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 3315,
+          "logTime": "2023-10-06 14:11:13"
         },
         {
-          id: 4,
-          type: "儲值",
-          station: "西園4",
-          seller: "56632513",
-          plate: "XXX-444",
-          account: "ABC444",
-          balance: 550,
-          invoice: "QW123456789",
-          amount: 500,
-          randonCode: "444",
-          issueDate: "2023-10-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
+          "chargeType": "儲值",
+          "name": "助安板橋殯儀館",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 2245,
+          "logTime": "2023-10-05 11:51:08"
         },
         {
-          id: 5,
-          type: "折抵",
-          station: "西園3",
-          seller: "56632513",
-          plate: "XXX-555",
-          account: "ABC555",
-          balance: 450,
-          invoice: "QW123456789",
-          amount: 50,
-          randonCode: "555",
-          issueDate: "2023-10-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
+          "chargeType": "扣抵",
+          "name": "助安板橋殯儀館",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 245,
+          "invoice": "TE43426244",
+          "totalAmount": 40,
+          "randomCode": "6244",
+          "invoiceDate": "2023-10-04",
+          "arrivalTime": "2023-10-04 10:41:55",
+          "logTime": "2023-10-04 11:31:36",
+          "Bidentifier": null,
+          "BName": null
         },
         {
-          id: 6,
-          type: "儲值",
-          station: "西園2",
-          seller: "56632513",
-          plate: "XXX-666",
-          account: "ABC666",
-          balance: 550,
-          invoice: "QW123456789",
-          amount: 500,
-          randonCode: "666",
-          issueDate: "2023-10-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
+          "chargeType": "扣抵",
+          "name": "助安板橋殯儀館",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 285,
+          "invoice": "TE43426017",
+          "totalAmount": 60,
+          "randomCode": "6017",
+          "invoiceDate": "2023-10-02",
+          "arrivalTime": "2023-10-02 13:22:05",
+          "logTime": "2023-10-02 14:28:58",
+          "Bidentifier": null,
+          "BName": null
         },
         {
-          id: 7,
-          type: "折抵",
-          station: "西園3",
-          seller: "56632513",
-          plate: "XXX-777",
-          account: "ABC777",
-          balance: 450,
-          invoice: "QW123456789",
-          amount: 50,
-          randonCode: "777",
-          issueDate: "2023-10-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
+          "chargeType": "扣抵",
+          "name": "助安板橋殯儀館",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 345,
+          "invoice": "TE43425979",
+          "totalAmount": 40,
+          "randomCode": "5979",
+          "invoiceDate": "2023-10-02",
+          "arrivalTime": "2023-10-02 10:25:59",
+          "logTime": "2023-10-02 11:03:49",
+          "Bidentifier": null,
+          "BName": null
         },
         {
-          id: 8,
-          type: "儲值",
-          station: "西園2",
-          seller: "56632513",
-          plate: "XXX-888",
-          account: "ABC888",
-          balance: 550,
-          invoice: "QW123456789",
-          amount: 500,
-          randonCode: "888",
-          issueDate: "2023-10-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
-        },
-        {
-          id: 9,
-          type: "折抵",
-          station: "西園3",
-          seller: "56632513",
-          plate: "XXX-999",
-          account: "ABC999",
-          balance: 450,
-          invoice: "QW123456789",
-          amount: 50,
-          randonCode: "999",
-          issueDate: "2023-10-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
-        },
-        {
-          id: 10,
-          type: "儲值",
-          station: "西園2",
-          seller: "56632513",
-          plate: "XXX-1010",
-          account: "ABC1010",
-          balance: 550,
-          invoice: "QW123456789",
-          amount: 500,
-          randonCode: "000",
-          issueDate: "2023-10-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
-        },
-        {
-          id: 11,
-          type: "折抵",
-          station: "西園3",
-          seller: "56632513",
-          plate: "XXX-1111",
-          account: "ABC1111",
-          balance: 450,
-          invoice: "QW123456789",
-          amount: 50,
-          randonCode: "1111",
-          issueDate: "2023-09-04",
-          entryTime: "2023-10-04 11:11:11",
-          departTime: "2023-10-04 11:11:11",
-          buyer: "12345678"
+          "chargeType": "扣抵",
+          "name": "助安板橋殯儀館",
+          "parkTax": "87545768",
+          "plate": "BGB-3832",
+          "user_acc": "99769806509643",
+          "newAmount": 385,
+          "invoice": "TE43401898",
+          "totalAmount": 30,
+          "randomCode": "1898",
+          "invoiceDate": "2023-09-28",
+          "arrivalTime": "2023-09-28 15:18:44",
+          "logTime": "2023-09-28 15:49:23",
+          "Bidentifier": null,
+          "BName": null
         },
       ],
       currentPage: 1,
@@ -193,6 +136,24 @@ export default {
     }
   },
   methods: {
+    getDetails() {
+      this.plate = atob(this.$route.params.plate);
+      const getDetailsApi = `${Api}/main/detail2`;
+      this.$http
+        .post(getDetailsApi, { plate: this.plate })
+        .then((response) => {
+          this.transactions = response.data.CHARGE_RECORD;
+          this.isLoading = false;
+        })
+    },
+    // 數字千分位格式
+    amountFormatter(row, column, cellValue) {
+      cellValue += '';
+      if (!cellValue.includes('.')) cellValue += '.';
+      return cellValue.replace(/(\d)(?=(\d{3})+\.)/g, function ($0, $1) {
+        return $1 + ',';
+      }).replace(/\.$/, '');
+    },
     handleCurrentChange(page) {
       this.currentPage = page;
     },
@@ -206,6 +167,9 @@ export default {
         this.currentPage--;
       }
     },
+  },
+  mounted() {
+    this.getDetails()
   },
   computed: {
     pageCount() {
@@ -232,7 +196,13 @@ export default {
       儲值扣抵明細
     </p>
   </div>
-  <div class="content d-flex justify-content-between">
+  <div v-if="isLoading" class="d-flex flex-column align-items-center">
+    <div>
+      <img src="../assets/loading.svg" alt="loading">
+    </div>
+    <div>努力加載中，請稍候...</div>
+  </div>
+  <div v-if="!isLoading" class="content mt-3 d-flex justify-content-between">
     <section>
       <span class="me-3">請選擇區間搜尋</span>
       <input type="date" class="pe-3 me-2" v-model="searchFrom">
@@ -244,24 +214,26 @@ export default {
       <button class="btn btn-primary text-white">匯出</button>
     </section>
   </div>
-  <div class="content mt-3">
-    <el-table :data="paginatedData" style="width: 100%;">
-      <el-table-column label="儲值種類" prop="type" align="center" width="80"></el-table-column>
-      <el-table-column label="場站" prop="station" align="center" width="100"></el-table-column>
-      <el-table-column label="賣方統編" prop="seller" align="center" width="100"></el-table-column>
+  <div v-if="!isLoading" class="content mt-3">
+    <el-table :data="paginatedData" class="el-table" style="font-size: 1rem;" stripe>
+      <el-table-column label="儲值種類" prop="chargeType" align="center" width="60"></el-table-column>
+      <el-table-column label="場站" prop="name" align="center" width="90"></el-table-column>
+      <el-table-column label="賣方統編" prop="parkTax" align="center" width="100"></el-table-column>
       <el-table-column label="車號" prop="plate" align="center" width="100"></el-table-column>
-      <el-table-column label="帳號" prop="account" align="center" width="100"></el-table-column>
-      <el-table-column label="帳戶餘額" prop="balance" align="center" width="100"></el-table-column>
+      <el-table-column label="帳號" prop="user_acc" align="center" width="90"></el-table-column>
+      <el-table-column label="帳戶餘額" prop="newAmount" align="center" width="100"
+        :formatter="amountFormatter"></el-table-column>
       <el-table-column label="發票號碼" prop="invoice" align="center" width="150"></el-table-column>
-      <el-table-column label="金額" prop="amount" align="center" width="80"></el-table-column>
-      <el-table-column label="隨機碼" prop="randonCode" align="center" width="80"></el-table-column>
-      <el-table-column label="發票日期" prop="issueDate" align="center" width="150"></el-table-column>
-      <el-table-column label="入場時間" prop="entryTime" align="center" width="200"></el-table-column>
-      <el-table-column label="變動時間" prop="departTime" align="center" width="200"></el-table-column>
-      <el-table-column label="買方統編" prop="buyer" align="center" width="100"></el-table-column>
+      <el-table-column label="金額" prop="totalAmount" align="center" width="80"
+        :formatter="amountFormatter"></el-table-column>
+      <el-table-column label="隨機碼" prop="randomCode" align="center" width="80"></el-table-column>
+      <el-table-column label="發票日期" prop="invoiceDate" align="center" width="150"></el-table-column>
+      <el-table-column label="入場時間" prop="arrivalTime" align="center" width="200"></el-table-column>
+      <el-table-column label="變動時間" prop="logTime" align="center" width="200"></el-table-column>
+      <el-table-column label="買方統編" prop="Bidentifier" align="center" width="100"></el-table-column>
     </el-table>
   </div>
-  <div class="content d-flex justify-content-center fixed-bottom">
+  <div v-if="!isLoading" class="content pagination">
     <!-- pagination -->
     <el-pagination @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[10, 20, 30, 40]"
       :page-size="pageSize" :total="transactions.length">
@@ -272,6 +244,17 @@ export default {
 <style>
 .content {
   max-width: 80%;
-  margin: 50px auto;
+  margin: 0 auto;
+}
+
+.el-table {
+  width: 100%;
+  height: 80%;
+}
+
+.pagination {
+  position: fixed;
+  top: 93%;
+  left: 35%;
 }
 </style>
