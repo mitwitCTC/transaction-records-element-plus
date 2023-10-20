4d9804a93e037e3f42b93c1006d13651cda2a0b2
eileen-qian <yiqianc.14@gmail.com>
2023-10-20 13:58:45 +0800

    update: 儲值扣抵明細搜尋、清除搜尋

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -131,12 +131,18 @@ export default {
       ],
       currentPage: 1,
       pageSize: 10,
-      searchFrom: '',
-      searchTo: ''
+      startDate: '',
+      endDate: '',
+      searchData: {
+        plate: '',
+        startDate: '',
+        endDate: '',
+      },
     }
   },
   methods: {
     getDetails() {
+      this.isLoading = true;
       this.plate = atob(this.$route.params.plate);
       const getDetailsApi = `${Api}/main/detail2`;
       this.$http
@@ -153,6 +159,31 @@ export default {
       return cellValue.replace(/(\d)(?=(\d{3})+\.)/g, function ($0, $1) {
         return $1 + ',';
       }).replace(/\.$/, '');
+    },
+    search() {
+      this.isLoading = true;
+      const searchApi = `${Api}/main/search`;
+      this.searchData = { plate: this.plate, startDate: this.startDate, endDate: this.endDate };
+      if (this.startDate == '' || this.endDate == '') {
+        alert('區間不得為空，請重新選擇日期區間搜尋');
+        this.isLoading = false;
+      } else if (this.startDate > this.endDate) {
+        alert('起始日不可晚於截止日，請重新選擇日期區間搜尋');
+        this.isLoading = false;
+      } else {
+        this.$http
+          .post(searchApi, this.searchData)
+          .then((response) => {
+            this.transactions = response.data.CHARGE_RECORD;
+            this.isLoading = false;
+          })
+      }
+    },
+    // 清除搜尋結果
+    clear(){
+      this.startDate = "";
+      this.endDate = "";
+      this.getDetails();
     },
     handleCurrentChange(page) {
       this.currentPage = page;
@@ -180,6 +211,16 @@ export default {
       const end = start + this.pageSize;
       return this.transactions.slice(start, end);
     },
+    // 搜尋區間最大為今日起往前推算三個月
+    currentDate() {
+      const today = new Date();
+      return today.toISOString().slice(0, 10);
+    },
+    maxSearchFromDate() {
+      const today = new Date();
+      const maxDate = new Date(today.getFullYear(), today.getMonth() - 2, today.getDate());
+      return maxDate.toISOString().slice(0, 10);
+    },
   },
   components: {
     ElTable,
@@ -205,10 +246,11 @@ export default {
   <div v-if="!isLoading" class="content mt-3 d-flex justify-content-between">
     <section>
       <span class="me-3">請選擇區間搜尋</span>
-      <input type="date" class="pe-3 me-2" v-model="searchFrom">
+      <input type="date" class="pe-3 me-2" v-model="startDate" :min="maxSearchFromDate" :max="currentDate">
       <span>～ </span>
-      <input type="date" class="pe-3 me-2" v-model="searchTo">
-      <button class="btn btn-primary text-white">搜尋</button>
+      <input type="date" class="pe-3 me-2" v-model="endDate" :min="maxSearchFromDate" :max="currentDate">
+      <button class="btn btn-primary text-white" @click="search">搜尋</button>
+      <button class="btn btn-outline-primary" style="margin-left: 10px;" @click="clear">清除搜尋結果</button>
     </section>
     <section>
       <button class="btn btn-primary text-white">匯出</button>
@@ -224,8 +266,7 @@ export default {
       <el-table-column label="帳戶餘額" prop="newAmount" align="center" width="100"
         :formatter="amountFormatter"></el-table-column>
       <el-table-column label="發票號碼" prop="invoice" align="center" width="150"></el-table-column>
-      <el-table-column label="金額" prop="totalAmount" align="center" width="80"
-        :formatter="amountFormatter"></el-table-column>
+      <el-table-column label="金額" prop="totalAmount" align="center" width="80"></el-table-column>
       <el-table-column label="隨機碼" prop="randomCode" align="center" width="80"></el-table-column>
       <el-table-column label="發票日期" prop="invoiceDate" align="center" width="150"></el-table-column>
       <el-table-column label="入場時間" prop="arrivalTime" align="center" width="200"></el-table-column>
